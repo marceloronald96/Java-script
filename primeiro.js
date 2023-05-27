14263225ad95f3af41adce25d85e3a18ceddff08
marceloronald96 <marceloronald96@gmail.com>
2023-05-26 21:22:12 -0300

    inicio ADA estudo de JS

---
+++ primeiro.js
@@ -0,0 +1,4 @@
+//Estou imprimindo duas mensagens no terminal//
+
+console.log ("Hello, World!")
+console.log ("Bem vindo ao curso Java Script!")
---
+++ variaveis.js
@@ -0,0 +1,28 @@
+//VARIÁVEIS//
+let idade = 26  //criando variável
+
+console.log (idade)
+
+// Tipos de variaveis
+
+idade = 30;    //Number
+let numero = 30;   // Number
+let altura = 1.77;    //Number
+let nome = "Marcelo"   //String 'Sempre texto'
+let  estudando = true  //variavel boleano verdadeiro ou falso
+
+
+console.log (numero, typeof numero)
+console.log (altura, typeof altura)
+console.log (nome, typeof nome)
+console.log (estudando, typeof estudando)
+
+let outra;
+
+console.log (outra)
+
+let nula = null;
+
+console.log (nula)
+
+const constante = 100; //UNICO VALOR NÃO PODE SER ALTERADO
